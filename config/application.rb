b0856ebc5c6d88ae0009cdda82c03e64b9d55380
Benedict Schurwanz <benedict.schurwanz@gmail.com>
2016-09-23 16:48:12 -0500

    comment out line setting rspec as test framework, not working properly

--- config/application.rb
+++ config/application.rb
@@ -2,11 +2,11 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.generators.test_framework :rspec
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+# config.generators.test_framework :rspec
 
 module Marvl
   class Application < Rails::Application
