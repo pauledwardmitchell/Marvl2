4bf7c253ee2ad6e41b7fc9b2bac3dd07ec5222e6
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-09-24 10:16:44 -0500

    Add location model, Modify other models associations to accomodate location, Add 2 orgs 2 locations to seeds

--- db/migrate/20160924011025_create_organisations.rb
+++ db/migrate/20160924011025_create_organisations.rb
@@ -2,11 +2,6 @@ class CreateOrganisations < ActiveRecord::Migration[5.0]
   def change
     create_table :organisations do |t|
       t.string :name
-      t.string :street_address
-      t.string :city
-      t.string :state
-      t.integer :zip_code
-      t.string :phone
       t.datetime :membership_expiration
 
       t.timestamps
