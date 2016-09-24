68a55d1d91e1ca99a43f216b831568e70afde2d3
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-24 13:12:16 -0500

    login / logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create', as: :post_login
+  delete '/logout', to: 'sessions#destroy'
 end
