19a96f091f1e0d4649ad9c1c3d027735467686e4
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-25 10:23:53 -0500

    added basic authorization so that only fellow users can see a users profile, updated application controller to include helpers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
-    @reviews = @user.reviews.limit(5)
+  	if logged_in?
+    	@user = User.find(params[:id])
+    	@reviews = @user.reviews.limit(5)
+ 	 	else
+    	flash[:access] = "Unauthorized access, please contact an administrator if you believe this error is incorrect."
+    	redirect root
+    end
   end
-
-  def index
-    
-  end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,7 @@ module ApplicationHelper
     current_user != nil
   end 
 
-  #broke these into two helper methods with the anticipation that eventually we will need to check admin vs. user
   def current_user
-     User.find(session[:user_id])
+    User.find(session[:user_id])
   end
 end
