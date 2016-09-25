884a424385cae4d81c68b8ef8020c6de7f8a8237
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-25 11:54:33 -0500

    update user model with date format method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews.limit(5)
+    @expiration = @user.date_format(@user.membership_expiration)
   end
 
   def index
