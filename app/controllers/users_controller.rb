c4c31fd97758a763ffa40f600dbc64bc1181c094
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-25 10:53:28 -0500

    edit link if on own profile, link to form with prepopulated fields, route back to profile if updates successful

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  	unless @user.id == current_user.id
+			flash[:access] = "Unauthorized access, please contact an administrator if you believe this error is incorrect."
+      redirect_to :back
+		end
+  end
+
+  def update
+  	@user = User.find(params[:id])
+    if @user.id == current_user.id
+  		@user.update_attributes(title: params[:user][:title], first_name: params[:user][:first_name], last_name: params[:user][:last_name], email: params[:user][:email])
+	  	redirect_to @user
+    else
+      flash[:access] = "Unauthorized access, please contact an administrator if you believe this error is incorrect."
+      redirect_to @user
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :members
 
-  resources :users, only: [:show, :new, :create]
+  resources :users
 
 end
