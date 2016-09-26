1297243036b0db0157a4a8151ac183a7360f261d
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-26 16:06:15 -0500

    sample vendor page functional, full_address method for easy display

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -2,4 +2,8 @@ class Vendor < ApplicationRecord
   has_many :services
   has_many :categories, through: :services
   has_many :reviews
+
+  def full_address
+  	self.street_address + " " + self.city + " " + self.state + " " + self.zip_code.to_s
+  end
 end
