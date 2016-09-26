eb779096e5822fa2ef3fcef424df212cf1e4fe8d
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-26 15:57:06 -0500

    sample vendor routes and controller (under user controller)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -59,6 +59,10 @@ class UsersController < ApplicationController
     @ambassadors = Role.where(name: "Ambassador")
   end
 
+  def vendors
+    @vendors = Vendor.all.limit(5)
+  end 
+
   private
   def user_params
     params.require(:user).permit!
