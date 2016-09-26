6f0c9557e8cfa9f6c1758aa76b1cd3b1108a93b5
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-25 22:14:46 -0500

    decided to redirect to root, left drop down in place for now

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,8 +37,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
-      redirect_to @user
+      redirect_to root_path
+      flash[:new] = "An administrator will notify you when your account has been confirmed"
     else
       @errors = @user.errors.full_messages
       render "new"
