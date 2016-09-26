f77f038504fcce8c09b6886a70c857aab46a64f3
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-09-26 14:27:22 -0500

    Add new review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,24 @@ class ReviewsController < ApplicationController
   	@review = Review.find(params[:id])
   end
 
+  def new
+    @vendor = Vendor.find(params[:vendor_id])
+    @review = Review.new
+  end
+
+  def create
+  	@vendor = Vendor.find(params[:vendor_id])
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to @review
+    else
+      render 'new'
+    end 
+  end
+
+  private
+    def review_params
+      params.require(:review).permit(:title, :content, :rating, :flagged, :vendor_id, :user_id)
+    end
 end
