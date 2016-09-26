baa827262b189be7f20398bac51418983a444fbc
Laurie Skelly <laurieskelly@users.noreply.github.com>
2016-09-26 12:04:36 -0500

    added migration to add expiration date column to user table, default one year from account creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925200327) do
+ActiveRecord::Schema.define(version: 20160926170041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,8 +77,9 @@ ActiveRecord::Schema.define(version: 20160925200327) do
     t.string   "email"
     t.string   "password_digest"
     t.integer  "organisation_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                                   null: false
+    t.datetime "updated_at",                                   null: false
+    t.date     "membership_expiration", default: '2017-09-26'
   end
 
   create_table "vendors", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,8 @@ organisation_two = Organisation.create(name: "KIPP DC")
 Location.create(name: "Main campus", street_address: "8900 Georgia Ave", city: "Silver Spring", state: "MD", zip_code: "20910", phone: "(301) 587-1215", organisation_id: organisation.id)
 Location.create(name: "KIPP DC AIM Academy: Douglass Campus", street_address: "2600 Douglass Road SE", city: "Washington", state: "DC", zip_code: "20020", phone: "202-678-5477", organisation_id: organisation_two.id)
 
-coop = User.create(first_name: "Coop", last_name: "Shop", email: "coop@shop.com", password: "password", organisation_id: organisation.id, membership_expiration: "12/01/16")
-fake = User.create(first_name: 'Fake', last_name: 'Member', email: 'fake@person.com', password: 'fakeperson', organisation_id: organisation_two.id, membership_expiration: "09/26/17")
+coop = User.create(first_name: "Coop", last_name: "Shop", email: "coop@shop.com", password: "password", organisation_id: organisation.id, membership_expiration: "12/01/26")
+fake = User.create(first_name: 'Fake', last_name: 'Member', email: 'fake@person.com', password: 'fakeperson', organisation_id: organisation_two.id)
 
 member = Role.create name: "Member"
 admin = Role.create name: "Admin"
