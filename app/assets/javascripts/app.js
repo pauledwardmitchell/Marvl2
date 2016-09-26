551e27835931b3c966b86b3535990f52f3f06291
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-09-26 09:53:05 -0500

    Fix js race condition in app.js Add one more seed organisation and location

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@
   var geocoder;
   
   var markers = [];
-  var allLocations = getLocations();
+
 
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
@@ -15,56 +15,47 @@
     largeInfowindow = new google.maps.InfoWindow();
     var bounds = new google.maps.LatLngBounds();
 
-    for (var i = 0; i < allLocations[0].length; i++) {
-      createMarker(allLocations[0][i]);
-    }
+    $.ajax({url: 'locations/all', method: 'GET', dataType: 'json'})
+
+    .done(function(allLocations){
+      for (var i = 0; i < allLocations.length; i++) {
+        createMarker(allLocations[i]);
+      }
+    });
+
       // map.fitBounds(bounds);
   }
 
-   function createMarker(locationObject) {
-     var address = locationObject.street_address + ", " + locationObject.city + ", " + locationObject.state + ", " + locationObject.zip_code
-     geocoder.geocode( { 'address': address }, function(results) {
-         addressLatLng = results[0].geometry.location;
-         map.setCenter(addressLatLng);
-         var marker = new google.maps.Marker({
-             map: map,
-             title: locationObject.name,
-             position: addressLatLng,
-             animation: google.maps.Animation.DROP
-         });
-         markers.push(marker);
-         marker.addListener('click', function() {
-           populateInfoWindow(this, largeInfowindow);
-         });
-         // bounds.extend(markers[i].position);
-     });
-   }
-
-
-    function fetchLocationsData() {
-      return $.ajax({url: 'locations/all', method: 'GET', dataType: 'json'});
-    }
-
-    function getLocations() {
-      var locations = []
-      fetchLocationsData().done(function(response){  
-      locations.push(response)
+  function createMarker(locationObject) {
+    var address = locationObject.street_address + ", " + locationObject.city + ", " + locationObject.state + ", " + locationObject.zip_code
+    geocoder.geocode( { 'address': address }, function(results) {
+      addressLatLng = results[0].geometry.location;
+      // map.setCenter(addressLatLng);
+      var marker = new google.maps.Marker({
+        map: map,
+        title: locationObject.name,
+        position: addressLatLng,
+        animation: google.maps.Animation.DROP
       });
-      return locations
+      markers.push(marker);
+      marker.addListener('click', function() {
+        populateInfoWindow(this, largeInfowindow);
+      });
+      // bounds.extend(markers[i].position);
+    });
+  }
+
+
+  function populateInfoWindow(marker, infowindow) {
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+      infowindow.marker = marker;
+      infowindow.setContent('<div>' + marker.title + '</div>');
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick',function(){
+        infowindow.setMarker(null);
+      });
     }
-
-
-
-    function populateInfoWindow(marker, infowindow) {
-      // Check to make sure the infowindow is not already opened on this marker.
-      if (infowindow.marker != marker) {
-        infowindow.marker = marker;
-        infowindow.setContent('<div>' + marker.title + '</div>');
-        infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick',function(){
-          infowindow.setMarker(null);
-        });
-      }
-    }
+  }
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-Organisation.create!(name: "Silver Spring UMC")
+# Organisation.create!(name: "Silver Spring UMC")
 
-Location.create!(name: "Main campus", street_address: "8900 Georgia Ave", city: "Silver Spring", state: "MD", zip_code: "20910", phone: "(301) 587-1215", organisation_id: "1")
+# Location.create!(name: "Main campus", street_address: "8900 Georgia Ave", city: "Silver Spring", state: "MD", zip_code: "20910", phone: "(301) 587-1215", organisation_id: "1")
 
-Organisation.create!(name: "KIPP DC")
+# Organisation.create!(name: "KIPP DC")
 
-Location.create!(name: "KIPP DC AIM Academy: Douglass Campus", street_address: "2600 Douglass Road SE", city: "Washington", state: "DC", zip_code: "20020", phone: "202-678-5477", organisation_id: "2")
+# Location.create!(name: "KIPP DC AIM Academy: Douglass Campus", street_address: "2600 Douglass Road SE", city: "Washington", state: "DC", zip_code: "20020", phone: "202-678-5477", organisation_id: "2")
 
-User.create!(email: "coop@shop.com", password: "password", organisation_id: 1)
+Organisation.create!(name: "E. L. Haynes Public Charter School")
+
+Location.create!(name: "E. L. Haynes Public Charter Elementary School", street_address: "4501 Kansas Avenue, NW", city: "Washington", state: "DC", zip_code: "20011", phone: "(202) 667-4446)", organisation_id: "3")
