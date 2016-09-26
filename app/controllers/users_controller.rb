c39151390b43bc8246eeca677e18ef52610eb154
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-25 21:02:31 -0500

    user registration controller routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   	unless @user.id == current_user.id
 			flash[:access] = "Unauthorized access, please contact an administrator if you believe this error is incorrect."
-      redirect_to :back
+      redirect_back
 		end
   end
 
@@ -31,4 +31,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      @errors = @user.errors.full_messages
+      render "new"
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
+  end
 end
