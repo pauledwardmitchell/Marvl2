97eaf44240912f78644d2d765e7d304d50b67c05
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-27 10:33:09 -0500

    user mailer model with link and subject message

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+
+	def membership_approved(user)
+		@user = user
+		@url = "45.55.84.132/login"
+		mail(to: @user.email, subject: "Welcome to MARVL - Your Account has been Approved!")
 end
