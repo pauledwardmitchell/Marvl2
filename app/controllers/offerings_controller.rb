76af82c8810e40e702a0295503367766f9968e13
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-09-27 10:17:42 -0500

    Take out binding.pry

--- app/controllers/offerings_controller.rb
+++ app/controllers/offerings_controller.rb
@@ -3,7 +3,6 @@ class OfferingsController < ApplicationController
   def create
   	@vendor = Vendor.find(params[:vendor_id])
     @offering = Offering.new(offering_params)
-  	binding.pry
 
     if @offering.save
       redirect_to @vendor
