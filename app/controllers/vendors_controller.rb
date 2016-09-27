98f2648df4dcf07ce310c9264b49bde950558279
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-09-27 07:37:35 -0500

    Anynoe can add a new vendor

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -3,12 +3,28 @@ class VendorsController < ApplicationController
   def index
     @vendors = Vendor.all
     @categories = Category.all
+    @vendor = Vendor.new
   end
 
   def show
     @vendor = Vendor.find(params[:id])
     @reviews = @vendor.reviews
     @vote = Vote.new
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_params)
+
+    if @vendor.save
+      redirect_to @vendor
+    else
+      render 'index'
+    end
+  end
+
+  private
+  def vendor_params
+    params.require(:vendor).permit(:name, :street_address, :city, :state, :zip_code, :phone, :email, :website)
   end
 
 end
