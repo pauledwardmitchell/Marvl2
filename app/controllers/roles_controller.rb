7da7a149f9e75643617882eb1b47e2969cc65189
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-27 09:52:32 -0500

    roles controller for add new member role

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -28,4 +28,17 @@ class RolesController < ApplicationController
 			redirect_to @user
 		end
 	end
+
+	def member
+		@user = User.find(params[:user_id])
+		@role = Role.new(name: "Member")
+		if @role.save
+			@user.add_role(@role.id)
+			flash[:role] = "Membership approved"
+			redirect_to @user
+		else
+			@errors = @role.errors.full_messages
+			redirect_to @user
+		end
+	end
 end
