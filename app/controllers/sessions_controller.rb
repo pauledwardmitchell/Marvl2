1be478e713dc2c8272588bb71a2e3a0ad40c6aa3
Kaitlyn Bradshaw <kaitlynbradshaw@Kaitlyns-MacBook-Pro.local>
2016-09-29 10:52:05 -0500

    centered alerts, logout to homepage

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,6 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:notice] = "Logged Out"
-    redirect_to '/login'
+    redirect_to root_path
   end
 end
