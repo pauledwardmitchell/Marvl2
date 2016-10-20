ecd7c05ec50307c14cd725099e95736b23afa9b8
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-10-20 17:58:48 -0500

    Add method and if statement to review index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,6 +32,10 @@ module ApplicationHelper
     end
   end
 
+  def reviews_for_category?(category)
+    @reviews.select { |review| review.vendor.categories.include? category }.sort_by { |review| review[:created_at] }.take(3) != nil
+  end
+
 
 end
 
