23abe42ec30ffa8317383d6f4014f81cf4008390
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-10-22 19:29:50 -0500

    More refactoring of views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,7 @@ class ReviewsController < ApplicationController
   def index
     if logged_in?
       @categories = Category.all  
-      @reviews = Review.all
+      @all_reviews = Review.all
       @recent_reviews = Review.all.order(created_at: :desc).take(5)
     else
       flash[:access] = "Unauthorized access, please log in."
