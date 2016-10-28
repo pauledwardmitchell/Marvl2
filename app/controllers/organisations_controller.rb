079130fed36f359aeee0360775a5a6cc354199a4
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-10-28 14:44:32 -0500

    Add organisation functionality to admin's user page

---
+++ app/controllers/organisations_controller.rb
@@ -0,0 +1,23 @@
+class OrganisationsController < ApplicationController
+
+  def new
+    @organisation = Organisation.new
+  end
+
+  def create
+    @organisation = Organisation.new(organisation_params)
+    @user = User.find(current_user.id)
+
+    if @organisation.save
+      redirect_to @user
+    else
+      redirect_to new_organisation_path
+    end
+  end
+
+  private
+  def organisation_params
+    params.require(:organisation).permit(:name)
+  end
+
+end
