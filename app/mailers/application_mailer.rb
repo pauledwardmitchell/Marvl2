2fac62bf0247006adbf0b99e32601b6322cc13c8
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-11-21 17:11:52 -0600

    Add initializer for smtp

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'cpamarvlapp@gmail.com'
+  default from: 'refugeesthrive@gmail.com'
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "marvl.org" }
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
+
 end
