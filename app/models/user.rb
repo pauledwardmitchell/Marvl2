b235b1e2bcc2f56fe8f688037c45d993d5673191
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-11-21 12:24:06 -0600

    Add migration for password reset stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,4 +50,11 @@ class User < ApplicationRecord
     Location.find_by organisation_id: self.organisation.id
   end
 
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver_now
+end
+
 end
