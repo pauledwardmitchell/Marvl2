617d8cc029bdff79c20ab3789f5d3d4578b9d31e
Laurie Skelly <laurieskelly@users.noreply.github.com>
2016-11-21 12:48:13 -0600

    member data seed script now seeds members

--- db/cpa_data/member_data.rb
+++ db/cpa_data/member_data.rb
@@ -1,5 +1,7 @@
 require 'CSV'
 
+
+# # # ORGS & LOCATIONS
 locations_data = CSV.read('db/cpa_data/locations_with_zip.csv')
 headers = locations_data.shift.collect {|h| h.to_sym}
 
@@ -14,23 +16,24 @@ locations_data.each do |row|
 
   # ORG
   # if this is the same org as last time just skip to the location
-  unless entry[:LegalName] == orgname
+  unless entry[:LegalName].strip == orgname
     # # find or create the organization in the db
-    orgname = entry[:LegalName]
+    orgname = entry[:LegalName].strip
     org = Organisation.find_or_create_by name: orgname
     orgid = org.id
   end
 
   # LOCATION
+  
+  loc = {
+    name: entry[:LocationName],
+    street_address: entry[:Address],
+    city: entry[:City],
+    state: entry[:State],
+    zip_code: entry[:Zip],
+    organisation_id: orgid
+  }
   begin
-    loc = {
-      name: entry[:LocationName],
-      street_address: entry[:Address],
-      city: entry[:City],
-      state: entry[:State],
-      zip_code: entry[:Zip],
-      organisation_id: orgid
-    }
     Location.create! loc
   rescue ActiveRecord::RecordInvalid
     invalid += 1
@@ -38,8 +41,47 @@ locations_data.each do |row|
   end
 end
 
-puts "number of invalid rows: #{invalid}"
-# people_data = CSV.read('db/cpa_data/members.csv')
 
-# headers = people_data.shift
 
+# # # PEOPLE 
+member_role_id = Role.find_by(name: "Member").id
+people_data = CSV.read('db/cpa_data/members.csv')
+people_headers = people_data.shift.collect {|h| h.to_sym}
+
+invalid_people = 0
+people_data.each do |row|
+
+  person = people_headers.zip(row).to_h
+  unless User.find_by email: person[:Email] 
+    # find their org
+    person_org = Organisation.find_by(name: person[:LegalName]).id
+
+    # make their temporary password
+    first_lower = person[:FirstName].downcase
+    temporary_password = "#{first_lower},changeyourpassword!"
+
+    new_user = {
+      title: person[:Role],
+      first_name: person[:FirstName],
+      last_name: person[:LastName],
+      email: person[:Email], 
+      password: temporary_password,
+      organisation_id: person_org
+    }
+    begin
+      user = User.create! new_user
+    rescue ActiveRecord::RecordInvalid
+      invalid_people += 1
+      puts "Record creation failed: \n#{new_user}"
+    end
+    if user
+      Privacy.create user_id: user.id
+      user.add_role(member_role_id)
+    end
+  end
+
+end
+
+
+puts "number of invalid location rows: #{invalid}"
+puts "number of invalid person rows: #{invalid_people}"
