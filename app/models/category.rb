edd223786742121f298efa6bfc12abfa03f943da
Paul Mitchell <pauledwardmitchell@gmail.com>
2016-11-28 17:23:55 -0600

    Add validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   has_many :offerings
   has_many :vendors, through: :offerings
   has_many :reviews, through: :vendors
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,11 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :vendor
   has_many :votes
+
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates_associated :vendor
+  validates_associated :user
+
 end
