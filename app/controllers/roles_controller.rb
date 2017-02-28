78d467ae4b6030accd39d0c556192874372f957d
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-02-28 16:11:53 -0600

    Add autoapprove function

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,14 +1,15 @@
 class RolesController < ApplicationController
 
 	def new
-    @roles = Role.new
+      @roles = Role.new
 	end
 
 	def create
-		@user = User.find(current_user.id)
-		@role = Role.find_by(name: "Ambassador")
-		@user.add_role(@role.id)
-    redirect_to @user
+	  @user = User.find(current_user.id)
+	  @role = Role.find_by(name: "Ambassador")
+	  @user.add_role(@role.id)
+      
+      redirect_to @user
 	end
 
 	def admin
