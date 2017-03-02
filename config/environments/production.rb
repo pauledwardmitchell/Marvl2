6a52d5e3bf71a4b6a85b7b7b8958a6b1b380fd79
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-03-02 13:34:38 -0600

    Add domain to smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :domain               => "marvl.org",
+   :user_name            => ENV['GMAIL_USERNAME'],
+   :password             => ENV['GMAIL_PASSWORD'],
    :authentication       => "plain",
    :enable_starttls_auto => true
   }
