4ef2396a83634711b5759c141c0ca8b01242bda7
Paul Mitchell <pauledwardmitchell@gmail.com>
2017-04-25 16:57:22 -0500

    Add asset host to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "www.marvl.org" }
 
+  config.action_mailer.asset_host = 'www.marvl.org'
+
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
