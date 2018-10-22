eb7dac1c41e34c3db9023aa4a347d3a5d7c178c6
Paul Mitchell <pauledwardmitchell@gmail.com>
2018-10-22 14:07:43 -0400

    Add table for all reviews

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Marvl
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_dispatch.default_headers = {
-      'Access-Control-Allow-Origin' => 'http://localhost:3000',
+      'Access-Control-Allow-Origin' => 'https://marvl-next.herokuapp.com',
       'Access-Control-Request-Method' => %w{GET}
     }
   end
